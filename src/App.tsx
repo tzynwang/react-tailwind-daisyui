eb62eec14f028a6df4f1458feb8455cf1a57f24a
Tzu Yin <tzyn.wang@gmail.com>
2022-05-12 23:11:42 +0800

    [chore] Add tailwind, daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function App(): React.ReactElement {
+  // States
+  const [theme, setTheme] = useState<'garden' | 'coffee'>('coffee');
+
+  // Functions
+  const handleThemeToggle = (): void => {
+    setTheme(prev => prev !== 'garden' ? 'garden' : 'coffee');
+  }
+
+  // Hooks
+  useEffect(() => {
+    const h = document.querySelector('html');
+    if (h) {
+      h.dataset.theme = theme;
+    }
+  }, [theme]);
+
+  // Main
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <button className="btn btn-primary" onClick={handleThemeToggle}>toggle theme between garden and coffee</button>
     </div>
   );
 }
